397f6fe48e94fe26eac9d0e4a3e6796ec397e530
andresovichh <ahenderson.holberton@gmail.com>
2022-01-13 12:55:45 -0300

    test

--- 5.foo_selector.c
+++ 5.foo_selector.c
@@ -21,10 +21,10 @@ instruction_t opcode_list[] = {
 {"nop", nop},
 };
 /** Get length of instruction list*/
-list_len = sizeof(opcode_list) / sizeof (instruction_t);
+list_len = sizeof(opcode_list) / sizeof(instruction_t);
 for (i = 0; i < list_len; i++)/**walk through instruction list*/
 {
-if (strcmp(token, opcode_list[i].opcode) == 0)/**if match, call corresponding fo*/
+if (strcmp(token, opcode_list[i].opcode) == 0)/**if match call fo*/
 {
 opcode_list[i].f(lifo, line_nbr);
 return (0);
