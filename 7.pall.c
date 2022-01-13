56fad735733c4ea1f00be359823238d5d89d3191
andresovichh <ahenderson.holberton@gmail.com>
2022-01-13 12:57:40 -0300

    test

--- 7.pall.c
+++ 7.pall.c
@@ -1,13 +1,14 @@
 #include "monty.h"
 
 /**
- * @brief 
- * 
+ * pall - prints all
+ * @lifo: data node
+ * @line_number: line number
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **lifo, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
+	stack_t *tmp = *lifo;
 	(void)line_number;
 
 	while (tmp)
