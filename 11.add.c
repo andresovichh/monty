80fae9083d5d98f2ba86fbff49529ea173f49856
andresovichh <ahenderson.holberton@gmail.com>
2022-01-13 21:06:59 -0300

    test

--- 11.add.c
+++ 11.add.c
@@ -7,12 +7,12 @@
  */
 void add(stack_t **lifo, unsigned int line_number)
 {
-	stack_t *tmp;
+stack_t *tmp;
 
-	if (*lifo == NULL || (*lifo)->next == NULL)
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+if (*lifo == NULL || (*lifo)->next == NULL)
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 
-	tmp = (*lifo)->next;
-	tmp->n += (*lifo)->n;
-	pop(lifo, line_number);
+tmp = (*lifo)->next;
+tmp->n += (*lifo)->n;
+pop(lifo, line_number);
 }
