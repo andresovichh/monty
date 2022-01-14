dcf71421bf60ff601cec5e645b299bfe3212df17
andresovichh <ahenderson.holberton@gmail.com>
2022-01-13 23:37:54 -0300

    test

--- 7.pall.c
+++ 7.pall.c
@@ -9,9 +9,8 @@
 void pall(stack_t **lifo, unsigned int line_number)
 {
 stack_t *tmp = *lifo;
+
 (void)line_number;
-if (*lifo == NULL)
-nop(lifo, line_number);
 while (tmp)
 {
 printf("%d\n", tmp->n);
