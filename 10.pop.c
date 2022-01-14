b83e39e40783865b1b2267a69f04bdc1bdcdb655
andresovichh <ahenderson.holberton@gmail.com>
2022-01-13 22:46:28 -0300

    test

--- 10.pop.c
+++ 10.pop.c
@@ -11,8 +11,10 @@ stack_t *tmp;
 
 tmp = *lifo;
 if (*lifo == NULL)
+{
 fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-
+exit(EXIT_FAILURE);
+}
 tmp = tmp->next;
 free(*lifo);
 *lifo = tmp;
