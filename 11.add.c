c428bff4b977719ace4f500c20edf72f3f444bb0
marcosozhol <3956@holbertonschool.com>
2022-01-16 07:43:13 -0800

    Correcciones

--- 11.add.c
+++ 11.add.c
@@ -10,7 +10,10 @@ void add(stack_t **lifo, unsigned int line_number)
 stack_t *tmp;
 
 if (*lifo == NULL || (*lifo)->next == NULL)
-fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
 
 tmp = (*lifo)->next;
 tmp->n += (*lifo)->n;
